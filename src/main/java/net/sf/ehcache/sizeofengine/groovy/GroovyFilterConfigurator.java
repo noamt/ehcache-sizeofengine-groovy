2f779ce9b665053d437abacb5d4f62a7abbd4ce0
Alex Snaps <alex.snaps@gmail.com>
2013-12-12 11:31:05 -0500

    Initial work, ignores all groovy.lang.MetaClass instances

---
+++ src/main/java/net/sf/ehcache/sizeofengine/groovy/GroovyFilterConfigurator.java
@@ -0,0 +1,14 @@
+package net.sf.ehcache.sizeofengine.groovy;
+
+import net.sf.ehcache.sizeofengine.Filter;
+import net.sf.ehcache.sizeofengine.FilterConfigurator;
+
+/**
+ * @author Alex Snaps
+ */
+public class GroovyFilterConfigurator implements FilterConfigurator {
+  @Override
+  public void configure(final Filter filter) {
+    filter.ignoreInstancesOf(groovy.lang.MetaClass.class, false);
+  }
+}
